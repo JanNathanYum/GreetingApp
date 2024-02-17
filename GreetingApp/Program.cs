813d0ebf93399bcb8c35cf57007b1a37f7a1dbef
yumol.301329 <yumol.301329@LAB-A-PC06>
2024-02-17 09:38:03 +0800

    Add project files.

---
+++ GreetingApp/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace GreetingApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            String name;
+            name = Console.ReadLine();
+            Console.Write("Enter the total number of your enrolled courses: ");
+            int courses;
+            courses = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the the price of your favorite book: ");
+            double book;
+            book = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Name: " + name);
+            Console.WriteLine("Total enrolled courses: " + courses);
+            Console.WriteLine("Price of the book: "+  book);
+        }
+    }
+}
